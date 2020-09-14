1fbbb78e9cf222d04889b73ac6fb4a86c962738d
root <root@ip-172-31-27-0.us-east-2.compute.internal>
2020-09-14 04:21:17 +0000

    UpdatedJava

--- Hello.java
+++ Hello.java
@@ -3,7 +3,10 @@ import java.util.*;
 class Hello{
 	public static void main(String[] args){
 		for(int i=0;i<10;i++){
-			System.out.println("*******");
+			if(i%2==0)
+				System.out.println("#*#*#*#*#*");
+			else
+				System.out.println("*#*#*#*#*#");
 		}
 	}
 }
